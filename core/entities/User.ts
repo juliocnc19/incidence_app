e015d1940253039b82fb7455e61453c83d446317
juliocnc19 <juliocnietoc19@gmail.com>
2025-03-24 14:01:48 -0400

    [ADD] descarte de arquitectura

---
+++ core/entities/User.ts
@@ -0,0 +1,12 @@
+export default interface User {
+  id: number
+  first_name: string
+  last_name: string
+  email: string
+  username: string
+  password: string
+  avatar_url: string
+  role_id: string
+  created_at?: string
+  updated_at?: string
+}
---
+++ utils/constants/endpoints.ts
@@ -0,0 +1,4 @@
+export const endpoints = {
+  login:"localhost:3001/auth/",
+  register:"localhost:3001/auth/register"
+}
