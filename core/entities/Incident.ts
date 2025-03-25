08b7d6a0a906bd4dbee215b50722a1e6f36a2e8a
juliocnc19 <juliocnietoc19@gmail.com>
2025-03-25 14:46:12 -0400

    [ADD] cambios varios

---
+++ core/entities/Incident.ts
@@ -0,0 +1,11 @@
+export interface Incident {
+  id: number
+  title: string
+  description: string
+  attachment_id: number
+  status_id: number
+  response: string
+  user_id: number
+  created_at?: string
+  updated_at?: string
+}
--- presentation/screens/Login.tsx
+++ presentation/screens/Login.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router"
-import { View} from "react-native"
+import { View } from "react-native"
 
 const LoginScreen = () => {
   return (
